23f80c72bd6a2a728c17f7b1bcbc387635189452
kotlinc <131211966+kotlinc@users.noreply.github.com>
2023-11-08 07:27:24 +1100

    OH COME ON

--- snake.js
+++ snake.js
@@ -4,20 +4,21 @@ const board_background = "white";
 const snake_col = 'lightblue';
 const snake_border = 'darkblue';
 const snakeboard = document.getElementById("game");
+const sqrSize = snakeboard.height / 40
 
 let snake = [
     { x: snakeboard.width / 2, y: snakeboard.height / 2 },
-    { x: snakeboard.width / 2 - 10, y: snakeboard.height / 2 },
-    { x: snakeboard.width / 2 - 20, y: snakeboard.height / 2 },
-    { x: snakeboard.width / 2 - 30, y: snakeboard.height / 2 },
-    { x: snakeboard.width / 2 - 40, y: snakeboard.height / 2 }
+    { x: snakeboard.width / 2 - sqrSize, y: snakeboard.height / 2 },
+    { x: snakeboard.width / 2 - sqrSize * 2, y: snakeboard.height / 2 },
+    { x: snakeboard.width / 2 - sqrSize * 3, y: snakeboard.height / 2 },
+    { x: snakeboard.width / 2 - sqrSize * 4, y: snakeboard.height / 2 }
 ]
 
 let score = 0;
 let changing_direction = false;
 let food_x;
 let food_y;
-let dx = 10;
+let dx = sqrSize;
 let dy = 0;
 
 const snakeboard_ctx = snakeboard.getContext("2d");
@@ -41,17 +42,17 @@ function reset() {
         clear_board();
         snake = [
             { x: snakeboard.width / 2, y: snakeboard.height / 2 },
-            { x: snakeboard.width / 2 - 10, y: snakeboard.height / 2 },
-            { x: snakeboard.width / 2 - 20, y: snakeboard.height / 2 },
-            { x: snakeboard.width / 2 - 30, y: snakeboard.height / 2 },
-            { x: snakeboard.width / 2 - 40, y: snakeboard.height / 2 }
+            { x: snakeboard.width / 2 - sqrSize, y: snakeboard.height / 2 },
+            { x: snakeboard.width / 2 - sqrSize * 2, y: snakeboard.height / 2 },
+            { x: snakeboard.width / 2 - sqrSize * 3, y: snakeboard.height / 2 },
+            { x: snakeboard.width / 2 - sqrSize * 4, y: snakeboard.height / 2 }
         ]
         document.getElementById('score').innerHTML = score;
         score = 0;
         changing_direction = false;
         food_x;
         food_y;
-        dx = 10;
+        dx = sqrSize;
         dy = 0;
         main();
     }, 3000)
@@ -71,7 +72,7 @@ function main() {
             drawSnake();
             // I AM RECURSION
             main();
-        }, 100)
+        }, sqrSize0)
     }
 }
 
@@ -89,15 +90,15 @@ function drawSnake() {
 function drawFood() {
     snakeboard_ctx.fillStyle = 'lightgreen';
     snakeboard_ctx.strokestyle = 'darkgreen';
-    snakeboard_ctx.fillRect(food_x, food_y, snakeboard.height / 40, snakeboard.height / 40);
-    snakeboard_ctx.strokeRect(food_x, food_y, snakeboard.height / 40, snakeboard.height / 40);
+    snakeboard_ctx.fillRect(food_x, food_y, sqrSize, sqrSize);
+    snakeboard_ctx.strokeRect(food_x, food_y, sqrSize, sqrSize);
 }
 
 function drawSnakePart(snakePart) {
     snakeboard_ctx.fillStyle = snake_col;
     snakeboard_ctx.strokestyle = snake_border;
-    snakeboard_ctx.fillRect(snakePart.x, snakePart.y, snakeboard.height / 40, snakeboard.height / 40);
-    snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, snakeboard.height / 40, snakeboard.height / 40);
+    snakeboard_ctx.fillRect(snakePart.x, snakePart.y, sqrSize, sqrSize);
+    snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, sqrSize, sqrSize);
 }
 
 function has_game_ended() {
@@ -105,19 +106,19 @@ function has_game_ended() {
         if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
     }
     const hitLeftWall = snake[0].x < 0;
-    const hitRightWall = snake[0].x > snakeboard.width - snakeboard.height / 40;
+    const hitRightWall = snake[0].x > snakeboard.width - sqrSize;
     const hitToptWall = snake[0].y < 0;
-    const hitBottomWall = snake[0].y > snakeboard.height - snakeboard.height / 40;
+    const hitBottomWall = snake[0].y > snakeboard.height - sqrSize;
     return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall
 }
 
 function random_food(min, max) {
-    return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+    return Math.round((Math.random() * (max - min) + min) / sqrSize) * sqrSize;
 }
 
 function gen_food() {
-    food_x = random_food(0, snakeboard.width - 10);
-    food_y = random_food(0, snakeboard.height - 10);
+    food_x = random_food(0, snakeboard.width - sqrSize);
+    food_y = random_food(0, snakeboard.height - sqrSize);
     snake.forEach(function has_snake_eaten_food(part) {
         const has_eaten = part.x == food_x && part.y == food_y;
         if (has_eaten) gen_food();
@@ -133,62 +134,62 @@ function change_direction(event) {
     if (changing_direction) return;
     changing_direction = true;
     const keyPressed = event.keyCode;
-    const goingUp = dy === -10;
-    const goingDown = dy === 10;
-    const goingRight = dx === 10;
-    const goingLeft = dx === -10;
+    const goingUp = dy === -sqrSize;
+    const goingDown = dy === sqrSize;
+    const goingRight = dx === sqrSize;
+    const goingLeft = dx === -sqrSize;
     if (keyPressed === LEFT_KEY && !goingRight) {
-        dx = -10;
+        dx = -sqrSize;
         dy = 0;
     }
     if (keyPressed === UP_KEY && !goingDown) {
         dx = 0;
-        dy = -10;
+        dy = -sqrSize;
     }
     if (keyPressed === RIGHT_KEY && !goingLeft) {
-        dx = 10;
+        dx = sqrSize;
         dy = 0;
     }
     if (keyPressed === DOWN_KEY && !goingUp) {
         dx = 0;
-        dy = 10;
+        dy = sqrSize;
     }
 }
 
 down.addEventListener('click', () => {
-    const goingUp = dy === 10;
+    const goingUp = dy === sqrSize;
 
     if (!goingUp) {
         dx = 0;
-        dy = 10;
+        dy = sqrSize;
     }
 })
 
 
 
 up.addEventListener('click', () => {
-    const goingDown = dy === -10;
+    const goingDown = dy === -sqrSize;
 
     if (!goingDown) {
         dx = 0;
-        dy = -10;
+        dy = -sqrSize;
     }
 })
 
 left.addEventListener('click', () => {
-    const goingRight = dx === 10;
+    const goingRight = dx === sqrSize;
 
     if (!goingRight) {
-        dx = -10;
+        dx = -sqrSize;
         dy = 0;
     }
 })
 
 right.addEventListener('click', () => {
-    const goingLeft = dx === -10;
+    const goingLeft = dx === -sqrSize;
 
     if (!goingLeft) {
-        dx = 10;
+        dx = sqrSize;
         dy = 0;
     }
 })
