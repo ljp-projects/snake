58fe6383748046f6eee6689ddb0e076daabdb10b
kotlinc <131211966+kotlinc@users.noreply.github.com>
2023-11-07 18:04:49 +1100

    you move or you lose

--- snake.js
+++ snake.js
@@ -16,17 +16,20 @@ const snakeboard_ctx = snakeboard.getContext("2d");
 
 let dx = 10;
 let dy = 0;
+let changing_direction = false
 
 main();
 
 function main() {
-    setTimeout(function onTick() {    
-     clearCanvas();    
-     moveSnake();  
-     drawSnake();
-     // I AM RECURSION
-     main();
-   }, 100)
+    if (has_game_ended()) return;
+    changing_direction = false;
+    setTimeout(function onTick() {
+        clearBoard();
+        moveSnake();
+        drawSnake();
+        // I AM RECURSION
+        main();
+    }, 100)
 }
 
 function clearCanvas() {
@@ -48,8 +51,58 @@ function drawSnakePart(snakePart) {
     snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
 
-function moveSnake() {  
-  const head = {x: snake[0].x + dx, y: snake[0].y + dy};
-  snake.unshift(head);
-  snake.pop();
+function moveSnake() {
+    const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+    snake.unshift(head);
+    snake.pop();
 }
+
+function changeDir(event) {
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+
+    const keyPressed = event.keyCode;
+    const goingUp = dy === -10;
+    const goingDown = dy === 10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;
+
+    if (keyPressed === LEFT_KEY && !goingRight) {
+        dx = -10;
+        dy = 0;
+    }
+
+    if (keyPressed === UP_KEY && !goingDown) {
+        dx = 0;
+        dy = -10;
+    }
+
+    if (keyPressed === RIGHT_KEY && !goingLeft) {
+        dx = 10;
+        dy = 0;
+    }
+
+    if (keyPressed === DOWN_KEY && !goingUp) {
+        dx = 0;
+        dy = 10;
+    }
+}
+
+function hasGameEnded() {
+    for (let i = 4; i < snake.length; i++) {
+        const has_collided = snake[i].x === snake[0].x && snake[i].y === snake[0].y
+        if (has_collided)
+            return true
+    }
+
+    const hitLeftWall = snake[0].x < 0;
+    const hitRightWall = snake[0].x > snakeboard.width - 10;
+    const hitToptWall = snake[0].y & lt; 0;
+    const hitBottomWall = snake[0].y > snakeboard.height - 10;
+
+    return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall
+}
+
+document.addEventListener("keydown", changeDir)
