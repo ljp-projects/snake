8a17d696a6ebc1d43ee39000f4cb743ec10368a5
kotlinc <131211966+kotlinc@users.noreply.github.com>
2023-11-07 17:57:33 +1100

    move

--- snake.js
+++ snake.js
@@ -1,7 +1,7 @@
 const board_border = 'black';
 const board_background = "white";
-const snake_col = 'lightred';
-const snake_border = 'darkred';
+const snake_col = 'white';
+const snake_border = 'black';
 
 let snake = [
     { x: 200, y: 200 },
@@ -16,8 +16,13 @@ const snakeboard_ctx = snakeboard.getContext("2d");
 main();
 
 function main() {
-    clearCanvas();
-    drawSnake();
+    setTimeout(function onTick() {    
+     clearCanvas();    
+     advanceSnake();  
+     drawSnake();
+     // I AM RECURSION
+     main();
+   }, 100)
 }
 
 function clearCanvas() {
@@ -37,4 +42,10 @@ function drawSnakePart(snakePart) {
     snakeboard_ctx.strokestyle = snake_border;
     snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
     snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function move_snake() {  
+  const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+  snake.unshift(head);
+  snake.pop();
 }
