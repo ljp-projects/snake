4db80cc14e365ef370ead15a0941acbeda3831af
kotlinc <131211966+kotlinc@users.noreply.github.com>
2023-11-09 07:49:30 +1100

    clear

--- snake.js
+++ snake.js
@@ -152,6 +152,7 @@ function main() {
         reset();
         return;
     } else if (paused) {
+        clear_canvas()
         const text = `Paused. Score: ${score}.`;
         snakeboard_ctx.fillText(text, snakeboard.width / 2.5, snakeboard.height / 2.5);
         main()
